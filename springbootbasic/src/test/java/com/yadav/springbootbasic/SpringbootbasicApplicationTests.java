e5f3e1c6c628a0b5fed09f27a61e977a83ab0223
Narendra Yadav <narendrayadav1988@gmail.com>
2018-09-13 12:29:34 +0530

    Spring boot basic application

---
+++ springbootbasic/src/test/java/com/yadav/springbootbasic/SpringbootbasicApplicationTests.java
@@ -0,0 +1,22 @@
+package com.yadav.springbootbasic;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootbasicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("SpringbootbasicApplicationTests initialization start at "+Calendar.getInstance().getTime());
+
+		System.out.println("SpringbootbasicApplicationTests initialization start at "+Calendar.getInstance().getTime());
+
+	}
+
+}
